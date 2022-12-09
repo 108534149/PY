feadb002acd0592b55b3a1eff784ceecc0e90461
翊 <113582342+108534149@users.noreply.github.com>
2022-12-09 10:56:21 +0800

    6

--- PY/相加.py
+++
@@ -1,8 +0,0 @@
-while True:  
-    #try試試看
-  try:  
-    數值= input().split(' ') #字串分割 
-    數值= [int(x) for x in 數值]  #轉數值
-    print (數值[0] + 數值[1])  
-  except (EOFError):  
-    break  
--- PY/矩陣相加.py
+++
@@ -1,7 +0,0 @@
-#輸入值=int(input("請輸入數字"))
-#質數=只有1和本身所以除完果為兩個數字
-輸入值=int(input("請輸入數字"))
-for x in range(1, 輸入值+ 1):
-    for j in range(1,x+1):
-        print(j,end=(""))
-    print()
